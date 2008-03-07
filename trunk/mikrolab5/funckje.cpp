4823171e3fa7da0f2b46a9239f1f04aeedb4d8ed
konrad.balys <konrad.balys@c17f3e5a-c547-0410-a969-25eb5a120115>
2008-03-07 23:15:37 +0000

    Wersja wstępnie działająca TODO: - komentarze i optymalizacja kodu asma - dodanie przeszukiwania w dwóch folderach - informacja o ilosci zduplikowanego miejsca przekazywana z funkcji - testowanie

--- trunk/mikrolab5/funckje.cpp
+++ trunk/mikrolab5/funckje.cpp
@@ -16,7 +16,9 @@ int search (wchar_t* biezacykat, HANDLE hFile) {
 	DWORD blad = 0;
 	wchar_t * sciezka = new wchar_t [MAX_PATH];
 	unsigned int dlugosc;
-	unsigned long int written = 0, rozmiar;
+	unsigned long int written = 0 ;
+	unsigned long long int rozmiar;
+
 	TElement * element = 0;
 	TKontener * kontener = 0;
 
@@ -40,10 +42,13 @@ int search (wchar_t* biezacykat, HANDLE hFile) {
 			// kropke znajduje przy findfirst
 			if ( !wcscmp(finddata.cFileName,L".."))
 				continue;
+			////////////////// pomocnicze
 			dlugosc = (unsigned int)wcsnlen(finddata.cFileName, MAX_PATH);
 			WriteFile(hFile,finddata.cFileName,dlugosc*2,&written,NULL);
 			// pisz enter
 			WriteFile(hFile,enter.enter,enter.dlugosc * 2,&written,NULL);
+
+			// pomocnicze
 			if (finddata.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY ){
 				wchar_t * nowasciezka;
 				// czy nazwa nie jest za dluga
@@ -74,6 +79,7 @@ int search (wchar_t* biezacykat, HANDLE hFile) {
 				wcsncpy(nowasciezka,biezacykat, MAX_PATH);
 				wcscat(nowasciezka,L"\\");
 				wcscat(nowasciezka,finddata.cFileName);
+				// okazuje sie ze unsigned long int ma 32 bity...
 				rozmiar = finddata.nFileSizeHigh;
 				rozmiar <<= 32;
 				rozmiar = finddata.nFileSizeLow;
@@ -104,11 +110,17 @@ int dodaj_kontener(unsigned long int rozmiar, TElement * element, wchar_t * scie
 	TKontener *temp = 0, *wstaw = 0;
 	// tworz element
 	element = 0;
+	if (!rozmiar) {
+		// pomin pliki o zerowej wielkosci
+		return 0;
+	}
 	element = new TElement;
 	if (!element) {
 		return 2; // blad
 	}
+
 	element->nast = 0;
+	element->zaznaczony = false;
 	wcscpy(element->sciezka, sciezka);
 	// pusto
 	if (!glowa_kontener) {
@@ -183,12 +195,18 @@ int dodaj_element(TKontener * kont, TElement * elem){
 
 
 DWORD WINAPI LiczMd5( LPVOID lpParam ) {
-	DWORD wynik;
+	DWORD wynik, wynik_obliczen;
 	while (1) {
 		wynik = WaitForSingleObject(Semafor,INFINITE);
 		if (wynik == WAIT_OBJECT_0) {
 			if (ilosc_zadan - nr_zadania){ // ilosc zadan do obsluzenia
-				CountMD5(zadania[nr_zadania]->sciezka,zadania[nr_zadania]->MD5);
+				wynik_obliczen = CountMD5(zadania[nr_zadania]->sciezka,zadania[nr_zadania]->MD5);
+				if (wynik_obliczen) {
+					// nie udalo sie otworzyc pliku przypisujemy mu niemozliwa sume 0
+					for(int i=0; i < 16; i++) {
+						zadania[nr_zadania]->MD5[i] = 0;
+					}
+				}
 				// wykonano 1 zadanie
 				nr_zadania++;
 				if (nr_zadania == ilosc_zadan || nr_zadania > MAKSYMALNA_ILOSC_ZADAN - 1){
@@ -245,11 +263,6 @@ void start(TOpcje & opcje){
 	if (ThreadSzukaj == NULL) {
         ExitProcess(1);
 	}
-	//WaitForSingleObject(hThread, INFINITE);
-	//SuspendThread(hThread);
-	//Sleep(5000);
-	//ResumeThread(hThread);
-
 	return ;
 }
 
@@ -281,10 +294,6 @@ int kasuj_elementy(TKontener * kontener){
 	return 0;
 }
 
-
-
-
-
 int dodaj_zadanie(TElement *element) {
 	DWORD wynik;
 	bool wykonuj = true;
@@ -306,14 +315,15 @@ int dodaj_zadanie(TElement *element) {
 	return 0;
 }
 
-int generuj_raport(TOpcje *opcje){
-	TKontener * glowa = glowa_kontener;
+int generuj_raport1(TOpcje *opcje){
 	HANDLE hPlik = INVALID_HANDLE_VALUE;
 	wchar_t WMD5[32+1];  // MD5 ma 16 bajtow, 32 znaki (char albo wchar_t)
 	unsigned long int written = 0;
 	unsigned long int dlugosc;
-	WMD5[32] = (WCHAR) 0; // dodaj zero na koniec
 	TElement * elem = 0;
+	TKontener * glowa = glowa_kontener;
+	
+	WMD5[32] = (WCHAR) 0; // dodaj zero na koniec
 	//hPlik = CreateFile((*(TOpcje*)lpParam).Raport, GENERIC_WRITE,  0, NULL, CREATE_ALWAYS, 0, NULL);			
 	hPlik = CreateFile(L"c:\\duplikaty.txt", GENERIC_WRITE,  0, NULL, CREATE_ALWAYS, 0, NULL);			
 	if (hPlik == INVALID_HANDLE_VALUE) {
@@ -325,7 +335,6 @@ int generuj_raport(TOpcje *opcje){
 		}
 		else {
 			elem = 0;
-
 		}
 		while(elem) {
 			dlugosc = (unsigned int)wcslen(elem->sciezka);
@@ -338,7 +347,6 @@ int generuj_raport(TOpcje *opcje){
 			dlugosc = (unsigned int)wcslen(L"\n\r");
 			WriteFile(hPlik,L"\r\n",dlugosc *2,&written,NULL);
 			elem = elem->nast;
-
 		}
 		glowa = glowa->nast;
 	}
@@ -346,16 +354,67 @@ int generuj_raport(TOpcje *opcje){
 	return 0;
 }
 
-/*
-/////////// przydzial pamieci
-        // Allocate memory for thread data.
-        pData = (typ) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,
-                sizeof(typ));
-      if( pData == NULL )
-           ExitProcess(2);
-	*/
 
-// konwersja input binarnego na znaki typu wide char
+int generuj_raport(TOpcje *opcje){
+	HANDLE hPlik = INVALID_HANDLE_VALUE;
+	wchar_t WMD5[32+1];  // MD5 ma 16 bajtow, 32 znaki (char albo wchar_t)
+	unsigned long int written = 0;
+	unsigned long int dlugosc;
+	TElement * elem = 0, *pod_elem = 0;
+	TKontener * glowa = glowa_kontener;
+	unsigned long long  int straty = 0;
+	
+	WMD5[32] = (WCHAR) 0; // dodaj zero na koniec
+	//hPlik = CreateFile((*(TOpcje*)lpParam).Raport, GENERIC_WRITE,  0, NULL, CREATE_ALWAYS, 0, NULL);			
+	hPlik = CreateFile(L"c:\\duplikaty.txt", GENERIC_WRITE,  0, NULL, CREATE_ALWAYS, 0, NULL);			
+	if (hPlik == INVALID_HANDLE_VALUE) {
+		return 1;
+	}
+	while (glowa) {
+		if (glowa->ilosc_elementow > 1){
+			elem = glowa->glowa;
+		}
+		else {
+			elem = 0;
+		}
+		while(elem) {
+			pod_elem = elem->nast;
+			while(pod_elem) {
+				if (!MD5Cmp(elem->MD5, pod_elem->MD5)) {
+					// sa co najmniej 2 identyczne pliki
+					if(!elem->zaznaczony){
+						// zapisz MD5 i pierwszy element
+						MD5ToWStr(WMD5,elem->MD5);
+						dlugosc = (unsigned int)wcslen(WMD5);
+						WriteFile(hPlik,WMD5,dlugosc * 2,&written,NULL);
+						WriteFile(hPlik,enter.enter,enter.dlugosc *2,&written,NULL);
+						dlugosc = (unsigned int)wcslen(elem->sciezka);
+						WriteFile(hPlik, elem->sciezka, dlugosc * 2,&written,NULL);
+						WriteFile(hPlik,enter.enter,enter.dlugosc *2,&written,NULL);
+						elem->zaznaczony = true;
+					}
+					if (!pod_elem->zaznaczony) {
+						dlugosc = (unsigned int)wcslen(pod_elem->sciezka);
+						WriteFile(hPlik, pod_elem->sciezka, dlugosc * 2,&written,NULL);
+						WriteFile(hPlik,enter.enter,enter.dlugosc *2,&written,NULL);
+						pod_elem->zaznaczony = true;
+						straty += glowa->rozmiar;
+					}
+				}
+				pod_elem = pod_elem->nast; // naste
+			}
+			elem = elem->nast; // element z ktorym porownujemy reszte listy elementow
+		}
+		glowa = glowa->nast; // przechodzenie po kontenerach
+	}
+	WriteFile(hPlik,(char*)&straty,8,&written,NULL);
+	CloseHandle(hPlik);
+	return 0;
+}
+
+
+
+// konwersja input (MD5) binarnego na znaki typu wide char
 void MD5ToWStr(wchar_t * output, char * input){
 	int i;
 	char temp[32];
@@ -366,7 +425,7 @@ void MD5ToWStr(wchar_t * output, char * input){
 	MultiByteToWideChar(CP_ACP,MB_PRECOMPOSED,temp, 32, output, 32);
 }
 
-// konwersja input binarnego na znaki typu char
+// konwersja input (MD5) binarnego na znaki typu char
 void MD5ToAStr(char * output, char * input){
 	int i;
 	for (i = 0; i < 16; i++ ) {
@@ -375,12 +434,17 @@ void MD5ToAStr(char * output, char * input){
 	}
 }
 
-
+int MD5Cmp(char *src1, char *src2){
+	int i = 0;
+	for(i = 0; i < 16; i++) {
+		if (src1[i] - src2[i])
+			return (src1[i] - src2[i]); // sa rozne
+	}
+	return 0; // sa rowne
+}
 
 
 
 
 
 ///////////////TODO
-// dodac warunek aby nie sprawdzal sumy kontrolnej dla pustych plikow
-// pliki, ktorych sie nie da otworzyc generuja bledna sume (nie oblicza sie dla niech smua)
